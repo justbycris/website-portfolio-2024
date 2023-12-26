a55d105a7975c97c6a5bde3228da6d6f46dc6ed5
Cristi Gutierrez <cristigtzname@gmail.com>
2023-12-26 08:57:53 -0600

    finish-contact

--- app.js
+++ app.js
@@ -20,3 +20,28 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+function copyText() {
+    // Get the text from the link
+    var emailAddress = document.getElementById("e-address");
+    emailAddress.value = 'cristigtzname@gmail.com'
+
+    // Create a temporary input element
+    var tempInput = document.createElement("input");
+    tempInput.value = emailAddress;
+
+    // Append the input element to the document
+    document.body.appendChild(tempInput);
+
+    // Select the text in the input element
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text to the clipboard
+    navigator.clipboard.writeText(emailAddress.value);
+
+    // Remove the temporary input element
+    document.body.removeChild(tempInput);
+
+    // Alert the user that the text has been copied (you can customize this part)
+    alert("Email address has been copied!");
+  }
